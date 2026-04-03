d3886d36cb9a8b483a6a31f568e8c4ab62238e99
Martijn Berger <mbr@veo.co>
2026-04-03 12:09:31 +0200

    build: add ferrocopg conninfo bootstrap facade

--- crates/ferrocopg-python/src/lib.rs
+++ crates/ferrocopg-python/src/lib.rs
@@ -1,7 +1,30 @@
 use pyo3::prelude::*;
 use pyo3::wrap_pyfunction;
+use pyo3::{exceptions::PyValueError, PyErr};
 
 const VERSION: &str = env!("CARGO_PKG_VERSION");
+
+#[pyclass(module = "ferrocopg_rust._ferrocopg")]
+struct BackendConninfoSummary {
+    #[pyo3(get)]
+    user: Option<String>,
+    #[pyo3(get)]
+    dbname: Option<String>,
+    #[pyo3(get)]
+    application_name: Option<String>,
+    #[pyo3(get)]
+    host_count: usize,
+    #[pyo3(get)]
+    hostaddr_count: usize,
+    #[pyo3(get)]
+    port_count: usize,
+    #[pyo3(get)]
+    has_password: bool,
+    #[pyo3(get)]
+    connect_timeout_seconds: Option<u64>,
+    #[pyo3(get)]
+    effective_connect_timeout_seconds: u64,
+}
 
 #[pyfunction]
 fn milestone() -> &'static str {
@@ -23,12 +46,37 @@ fn backend_core() -> &'static str {
     ferrocopg_postgres::backend_core()
 }
 
+#[pyfunction]
+fn parse_conninfo_summary(conninfo: &str) -> PyResult<BackendConninfoSummary> {
+    ferrocopg_postgres::bootstrap_summary(conninfo)
+        .map(BackendConninfoSummary::from)
+        .map_err(|err| PyErr::new::<PyValueError, _>(err.to_string()))
+}
+
+impl From<ferrocopg_postgres::ConninfoSummary> for BackendConninfoSummary {
+    fn from(summary: ferrocopg_postgres::ConninfoSummary) -> Self {
+        Self {
+            user: summary.user,
+            dbname: summary.dbname,
+            application_name: summary.application_name,
+            host_count: summary.host_count,
+            hostaddr_count: summary.hostaddr_count,
+            port_count: summary.port_count,
+            has_password: summary.has_password,
+            connect_timeout_seconds: summary.connect_timeout_seconds,
+            effective_connect_timeout_seconds: summary.effective_connect_timeout_seconds,
+        }
+    }
+}
+
 #[pymodule]
 fn _ferrocopg(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add("__version__", VERSION)?;
+    m.add_class::<BackendConninfoSummary>()?;
     m.add_function(wrap_pyfunction!(milestone, m)?)?;
     m.add_function(wrap_pyfunction!(scaffold_status, m)?)?;
     m.add_function(wrap_pyfunction!(backend_stack, m)?)?;
     m.add_function(wrap_pyfunction!(backend_core, m)?)?;
+    m.add_function(wrap_pyfunction!(parse_conninfo_summary, m)?)?;
     Ok(())
 }
