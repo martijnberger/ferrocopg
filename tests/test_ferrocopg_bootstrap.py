2eb599fd9caffe054396c9262baf53d4298d8a4c
Martijn Berger <mbr@veo.co>
2026-04-03 14:41:21 +0200

    port: add ferrocopg copy row helpers

--- tests/test_ferrocopg_bootstrap.py
+++ tests/test_ferrocopg_bootstrap.py
@@ -36,3 +36,86 @@ def test_ferrocopg_wrapper(monkeypatch):
         ("summary", "host=localhost"),
         ("plan", "host=localhost"),
     ]
+
+
+def test_copy_base_prefers_c_copy_optimizations(monkeypatch):
+    module = importlib.import_module("psycopg._copy_base")
+
+    class StubCModule:
+        @staticmethod
+        def format_row_text(*args: object) -> None:
+            pass
+
+        @staticmethod
+        def format_row_binary(*args: object) -> None:
+            pass
+
+        @staticmethod
+        def parse_row_text(*args: object) -> tuple[()]:
+            return ()
+
+        @staticmethod
+        def parse_row_binary(*args: object) -> tuple[()]:
+            return ()
+
+    class StubRustModule:
+        @staticmethod
+        def format_row_text(*args: object) -> None:
+            pass
+
+        @staticmethod
+        def format_row_binary(*args: object) -> None:
+            pass
+
+        @staticmethod
+        def parse_row_text(*args: object) -> tuple[()]:
+            return ()
+
+        @staticmethod
+        def parse_row_binary(*args: object) -> tuple[()]:
+            return ()
+
+    monkeypatch.setattr(module, "_psycopg", StubCModule)
+    monkeypatch.setattr(module, "_rpsycopg", StubRustModule)
+
+    format_row_text, format_row_binary, parse_row_text, parse_row_binary = (
+        module._load_copy_impl()
+    )
+
+    assert format_row_text is StubCModule.format_row_text
+    assert format_row_binary is StubCModule.format_row_binary
+    assert parse_row_text is StubCModule.parse_row_text
+    assert parse_row_binary is StubCModule.parse_row_binary
+
+
+def test_copy_base_uses_ferrocopg_copy_optimizations(monkeypatch):
+    module = importlib.import_module("psycopg._copy_base")
+
+    class StubRustModule:
+        @staticmethod
+        def format_row_text(*args: object) -> None:
+            pass
+
+        @staticmethod
+        def format_row_binary(*args: object) -> None:
+            pass
+
+        @staticmethod
+        def parse_row_text(*args: object) -> tuple[()]:
+            return ()
+
+        @staticmethod
+        def parse_row_binary(*args: object) -> tuple[()]:
+            return ()
+
+    monkeypatch.setattr(module, "_psycopg", None)
+    monkeypatch.setattr(module, "_rpsycopg", StubRustModule)
+
+    format_row_text, format_row_binary, parse_row_text, parse_row_binary = (
+        module._load_copy_impl()
+    )
+
+    assert format_row_text is StubRustModule.format_row_text
+    assert format_row_binary is StubRustModule.format_row_binary
+    assert parse_row_text is StubRustModule.parse_row_text
+    assert parse_row_binary is StubRustModule.parse_row_binary
