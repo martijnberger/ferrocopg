73d8ed92cb915882b70be3bd0292d41be36b3404
Martijn Berger <mbr@veo.co>
2026-04-04 09:39:56 +0200

    port: add ferrocopg prepared statement cache

--- crates/ferrocopg-python/src/bootstrap.rs
+++ crates/ferrocopg-python/src/bootstrap.rs
@@ -169,6 +169,15 @@ struct BackendStatementDescription {
     columns: Vec<BackendStatementColumn>,
 }
 
+#[derive(Clone)]
+#[pyclass(module = "ferrocopg_rust._ferrocopg")]
+struct BackendPreparedStatementInfo {
+    #[pyo3(get)]
+    statement_id: u64,
+    #[pyo3(get)]
+    description: BackendStatementDescription,
+}
+
 #[pyclass(module = "ferrocopg_rust._ferrocopg")]
 struct BackendSyncNoTlsSession {
     inner: Mutex<ferrocopg_postgres::SyncNoTlsSession>,
@@ -407,6 +416,15 @@ impl From<ferrocopg_postgres::StatementDescription> for BackendStatementDescript
     }
 }
 
+impl From<ferrocopg_postgres::PreparedStatementInfo> for BackendPreparedStatementInfo {
+    fn from(info: ferrocopg_postgres::PreparedStatementInfo) -> Self {
+        Self {
+            statement_id: info.statement_id,
+            description: info.description.into(),
+        }
+    }
+}
+
 #[pymethods]
 impl BackendSyncNoTlsSession {
     #[getter]
@@ -542,6 +560,78 @@ impl BackendSyncNoTlsSession {
             .map(BackendStatementDescription::from)
             .map_err(|err| PyErr::new::<pyo3::exceptions::PyRuntimeError, _>(err.to_string()))
     }
+
+    fn prepare_text(&self, query: &str) -> PyResult<BackendPreparedStatementInfo> {
+        self.inner
+            .lock()
+            .map_err(|_| {
+                PyErr::new::<pyo3::exceptions::PyRuntimeError, _>(
+                    "backend session mutex is poisoned",
+                )
+            })?
+            .prepare_text(query)
+            .map(BackendPreparedStatementInfo::from)
+            .map_err(|err| PyErr::new::<pyo3::exceptions::PyRuntimeError, _>(err.to_string()))
+    }
+
+    fn describe_prepared(&self, statement_id: u64) -> PyResult<BackendStatementDescription> {
+        self.inner
+            .lock()
+            .map_err(|_| {
+                PyErr::new::<pyo3::exceptions::PyRuntimeError, _>(
+                    "backend session mutex is poisoned",
+                )
+            })?
+            .describe_prepared(statement_id)
+            .map(BackendStatementDescription::from)
+            .map_err(|err| PyErr::new::<pyo3::exceptions::PyRuntimeError, _>(err.to_string()))
+    }
+
+    fn query_prepared_text_params(
+        &self,
+        statement_id: u64,
+        params: Vec<Option<String>>,
+    ) -> PyResult<BackendTextQueryResult> {
+        self.inner
+            .lock()
+            .map_err(|_| {
+                PyErr::new::<pyo3::exceptions::PyRuntimeError, _>(
+                    "backend session mutex is poisoned",
+                )
+            })?
+            .query_prepared_text_params(statement_id, &params)
+            .map(BackendTextQueryResult::from)
+            .map_err(|err| PyErr::new::<pyo3::exceptions::PyRuntimeError, _>(err.to_string()))
+    }
+
+    fn execute_prepared_text_params(
+        &self,
+        statement_id: u64,
+        params: Vec<Option<String>>,
+    ) -> PyResult<BackendExecuteResult> {
+        self.inner
+            .lock()
+            .map_err(|_| {
+                PyErr::new::<pyo3::exceptions::PyRuntimeError, _>(
+                    "backend session mutex is poisoned",
+                )
+            })?
+            .execute_prepared_text_params(statement_id, &params)
+            .map(BackendExecuteResult::from)
+            .map_err(|err| PyErr::new::<pyo3::exceptions::PyRuntimeError, _>(err.to_string()))
+    }
+
+    fn close_prepared(&self, statement_id: u64) -> PyResult<()> {
+        self.inner
+            .lock()
+            .map_err(|_| {
+                PyErr::new::<pyo3::exceptions::PyRuntimeError, _>(
+                    "backend session mutex is poisoned",
+                )
+            })?
+            .close_prepared(statement_id)
+            .map_err(|err| PyErr::new::<pyo3::exceptions::PyRuntimeError, _>(err.to_string()))
+    }
 }
 
 pub(crate) fn register(m: &Bound<'_, PyModule>) -> PyResult<()> {
@@ -555,6 +645,7 @@ pub(crate) fn register(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_class::<BackendStatementParameter>()?;
     m.add_class::<BackendStatementColumn>()?;
     m.add_class::<BackendStatementDescription>()?;
+    m.add_class::<BackendPreparedStatementInfo>()?;
     m.add_class::<BackendSyncNoTlsSession>()?;
     m.add_function(wrap_pyfunction!(milestone, m)?)?;
     m.add_function(wrap_pyfunction!(scaffold_status, m)?)?;
