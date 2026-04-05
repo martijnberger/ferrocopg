1252f3fc96f100406b2869718e9cadf5a849ab67
Martijn Berger <mbr@veo.co>
2026-04-05 18:37:13 +0200

    ci: restore windows libpq path export

--- tools/ci/wheel_win32_before_build.bat
+++ tools/ci/wheel_win32_before_build.bat
@@ -6,3 +6,14 @@ REM A specific version cannot be easily chosen.
 REM https://github.com/microsoft/vcpkg/discussions/25622
 vcpkg install libpq:x64-windows-release
 uv tool install --force .\tools\ci\pg_config_vcpkg_stub\
+
+for /f "delims=" %%i in ('pg_config --bindir') do set "LIBPQ_BINDIR=%%i"
+if not exist "%LIBPQ_BINDIR%\libpq.dll" (
+    echo ERROR: libpq.dll not found in %LIBPQ_BINDIR%
+    exit /b 1
+)
+
+if defined GITHUB_PATH (
+    >> "%GITHUB_PATH%" echo %LIBPQ_BINDIR%
+    for /f "delims=" %%i in ('pg_config --libdir') do >> "%GITHUB_PATH%" echo %%i
+)
